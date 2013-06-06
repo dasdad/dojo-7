fe2a4a1e0f103a2923b5846459bb4dbea76b63b8
Leandro Facchinetti <leafac@gmail.com>
2013-06-06 20:35:29 -0300

    Mudei toda a idea e agora tenho o oceano

---
+++ src/dojo/Oceano.java
@@ -0,0 +1,7 @@
+package dojo;
+
+public class Oceano {
+  public int areaCoberta(){
+    return 0;
+  }
+}
---
+++ test/dojo/OceanoTest.java
@@ -0,0 +1,14 @@
+package dojo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OceanoTest {
+
+  @Test
+  public void test() {
+    assertEquals(0, new Oceano().areaCoberta());
+  }
+
+}
